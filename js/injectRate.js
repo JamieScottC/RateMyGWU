a76688bf12962c92f67c3aa4f3012aaa8ad31504
Jamie Scott <scott.jams@gmail.com>
2020-07-22 00:08:03 -0400

    Tooltip popping up above teachers head

--- js/injectRate.js
+++ js/injectRate.js
@@ -1,1 +1,11 @@
 console.log("Testing");
+constructPage();
+function constructPage(){
+	//Loop through all the course rows
+	$('.crseRow1').each(function(i, obj) {
+    	console.log($(this).children().eq(6));
+    	$(this).children().eq(6).addClass("tooltip");
+    	$(this).children().eq(6).append("<div class='top'><h3>Lorem Ipsum</h3> <p>Dolor sit amet, consectetur adipiscing elit.</p> <i></i> </div>")
+	});
+
+}
